fdc038baefe92f68a015cd551b2900fc57ad25a4
Juliano Sirtori <julianosirtori@gmail.com>
2023-08-11 17:40:45 -0300

    feature: created KPI component

---
+++ src/components/KPI.tsx
@@ -0,0 +1,13 @@
+type KPIProps = {
+  label: string;
+  value: number;
+};
+
+export const KPI = ({ label, value }: KPIProps) => {
+  return (
+    <div className="flex w-full max-w-[125px] flex-col items-center justify-center  gap-3 rounded-sm bg-secondary-background py-4">
+      <span className="text-2xl font-bold text-primary-text">{value}</span>
+      <span className="font-medium text-secondary-text">{label}</span>
+    </div>
+  );
+};
