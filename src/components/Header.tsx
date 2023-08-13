01d3df821de6a3effe48afb7ba3088fcd4efaedc
Juliano Sirtori <julianosirtori@gmail.com>
2023-08-13 11:31:57 -0300

    feat: :sparkles: add links on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+
 export const Header = () => {
   return (
-    <header className="px-8 py-4">
+    <header className="flex flex-row items-center justify-between px-8 py-4 ">
       <h1 className="text-xl text-primary-text">Juliano`s Life in Weeks</h1>
+      <nav className="flex flex-row items-center gap-2">
+        <Link href="https://github.com/julianosirtori/life-in-weeks">
+          <GitHubLogoIcon className="h-6 w-6 text-secondary-text" />
+        </Link>
+        <Link href="https://www.linkedin.com/in/juliano-sirtori/">
+          <LinkedInLogoIcon className="h-6 w-6 text-secondary-text" />
+        </Link>
+      </nav>
     </header>
   );
 };
